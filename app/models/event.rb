4ef2c98089a38ee3881a6c2ebfb0951fd26b8cfe
Christian Salazar <calaca89@gmail.com>
2020-01-09 12:14:04 -0500

    Adding associations between attendees and attended events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :attendances
+  has_mane :attendees, though: :attendances, source: 'user'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :name, {:presence=>true}
   validates :email, presence: true
+  has_many :events, class_name: 'Event', foreing_key: 'user_id'
+  has_many :attendances
+  has_many :attended_events, through: :attendances, source: 'event'
 
-  has_many :events
-
-  has_secure_password
 end
